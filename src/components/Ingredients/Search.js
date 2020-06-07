e5f000b7bbfb14424419322b3690351074e1312b
Acostan13 <acostan@fau.edu>
2020-06-07 11:53:59 -0400

    added useCallback()

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -4,26 +4,27 @@ import Card from "../UI/Card";
 import "./Search.css";
 
 const Search = React.memo((props) => {
-  const { onLoadIngredients } = props
+  const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState("");
 
   useEffect(() => {
-    const query = enteredFilter.length === 0 
-    ? '' 
-    : `?orderBy="title"&equalTo="${enteredFilter}"`
+    const query =
+      enteredFilter.length === 0
+        ? ""
+        : `?orderBy="title"&equalTo="${enteredFilter}"`;
     fetch("https://react-hooks-update-ae798.firebaseio.com/ingredients.json" + query)
-    .then((response) => response.json())
-    .then((responseData) => {
-      const loadedIngredients = [];
-      for (const key in responseData) {
-        loadedIngredients.push({
-          id: key,
-          title: responseData[key].title,
-          amount: responseData[key].amount,
-        });
-      }
-      // onLoadIngredients(loadedIngredients)
-    });
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        onLoadIngredients(loadedIngredients);
+      });
   }, [enteredFilter, onLoadIngredients]);
 
   return (
