9361976332a40498941a103a9cefa58d18892fa4
Acostan13 <acostan@fau.edu>
2020-06-07 11:00:50 -0400

    loading data with useEffect()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,19 +7,37 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  // Gets called after this component gets rendered or rerendered
+  useEffect(() => {
+    fetch("https://react-hooks-update-ae798.firebaseio.com/ingredients.json")
+    .then((response) => response.json())
+    .then((responseData) => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount,
+        });
+      }
+      // setUserIngredients(loadedIngredients)
+    });
+  }, []);
   const addIngredientHandler = (ingredient) => {
     fetch("https://react-hooks-update-ae798.firebaseio.com/ingredients.json", {
       method: "POST",
       body: JSON.stringify(ingredient),
       headers: { "Content-Type": "application/json" },
-    }).then(response => {
-      return response.json()
-    }).then(responseData => {
-      setUserIngredients((prevIngredients) => [
-        ...prevIngredients,
-        { id: responseData.name, ...ingredient },
-      ]);
     })
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   const removeIngredientHandler = (ingredientId) => {
